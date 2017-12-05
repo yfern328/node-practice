81664bbb217560605fd7527998c9cb201d7509a9
yfern328 <yfernan2@u.rochester.edu>
2017-12-05 18:02:08 -0500

    testing out 3rd party module lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,17 +2,34 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+// TESTING lodash functions
 
-var res = notes.addNote();
+console.log(_.isString(true));
+console.log(_.isString('Andrew'));
 
-console.log(res);
-console.log('Result:', notes.add(9, -2));
+var filteredArray = _.uniq(['Yelstin', 1, 'Yelstin', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+//PRACTICE WORK
+
+//Testing node modules
+
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
+
+//Testing module exports in notes.js
+
+// var res = notes.addNote();
+//
+// console.log(res);
+
+// console.log('Result:', notes.add(9, -2));

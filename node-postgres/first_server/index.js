ae0216ffdc002e04751f4368027b2bb57c8ddda8
yfern328 <yfernan2@u.rochester.edu>
2017-12-11 23:42:17 -0500

    added gitignore, nodemon, and basic routing with JSON

--- node-postgres/first_server/index.js
+++ node-postgres/first_server/index.js
@@ -4,6 +4,18 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+
+  const { url } = req;
+
+  if (url === '/') {
+    const test = { 1: 'one', 2: 'two', 3: 'three'};
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify(test));
+    res.end();
+  }
+
+  console.log(url);
+
   res.end('Welcome to Node!');
 });
 

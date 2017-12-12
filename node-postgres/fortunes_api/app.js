829fd347bd7a14ec0bec83cd19ef6887414ddae5
yfern328 <yfernan2@u.rochester.edu>
2017-12-12 15:23:12 -0500

    can now make post requests to fortunes.json using curl, bodyparser, and fs module

--- node-postgres/fortunes_api/app.js
+++ node-postgres/fortunes_api/app.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 const fortunes = require('./data/fortunes');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/fortunes', (req, res) => {
   res.json(fortunes);
@@ -15,4 +19,28 @@ app.get('/fortunes/:id', (req, res) => {
   res.json(fortunes.find(f => f.id == req.params.id));
 });
 
+app.post('/fortunes', (req, res) => {
+  console.log(req.body)
+
+  const { message, lucky_number, spirit_animal } = req.body;
+
+  const fortune_ids = fortunes.map(f => f.id);
+
+  const fortune = {
+    id: (fortune_ids.length > 0 ? Math.max(...fortune_ids) : 0) + 1,
+    message,
+    lucky_number,
+    spirit_animal
+  };
+
+  const new_fortunes = fortunes.concat(fortune);
+
+  fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes, undefined, 2), err => console.log(err));
+
+  res.json(new_fortunes);
+
+});
+
 module.exports = app;
+
+// curl -H "Content-Type: application/json" -X POST -d '{"message": "Hello", "lucky_number": 5, "spirit_animal": "Dog"}' http://localhost:3000/fortunes

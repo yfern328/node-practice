e20612cc71b231ccc58ffae61e5c2a68b0d8c986
yfern328 <yfernan2@u.rochester.edu>
2017-12-12 00:14:58 -0500

    basic express setup with simple endpoint and json data

---
+++ node-postgres/fortunes_api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const fortunes = require('./data/fortunes');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/fortunes', (req, res) => {
+  res.json(fortunes);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));

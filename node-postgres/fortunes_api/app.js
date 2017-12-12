4e308ca343ccadfdb6ffef8852df3863b73223c6
yfern328 <yfernan2@u.rochester.edu>
2017-12-12 14:42:51 -0500

    added random fortune endpoint and endpoint by id

--- node-postgres/fortunes_api/app.js
+++ node-postgres/fortunes_api/app.js
@@ -7,4 +7,12 @@ app.get('/fortunes', (req, res) => {
   res.json(fortunes);
 });
 
+app.get('/fortunes/random', (req, res) => {
+  res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
+});
+
+app.get('/fortunes/:id', (req, res) => {
+  res.json(fortunes.find(f => f.id == req.params.id));
+});
+
 module.exports = app;

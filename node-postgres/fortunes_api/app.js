29c8924e93af90fcabe8f8f3e9732140318e76e1
yfern328 <yfernan2@u.rochester.edu>
2017-12-12 16:45:04 -0500

    can make put requests for full or parital edits, refactoring

--- node-postgres/fortunes_api/app.js
+++ node-postgres/fortunes_api/app.js
@@ -5,6 +5,7 @@ const fortunes = require('./data/fortunes');
 
 const app = express();
 
+//bodyParser is middleware that will expose body of incoming request in req.body. Gives access to POST requests.
 app.use(bodyParser.json());
 
 app.get('/fortunes', (req, res) => {
@@ -19,28 +20,51 @@ app.get('/fortunes/:id', (req, res) => {
   res.json(fortunes.find(f => f.id == req.params.id));
 });
 
+const writeFortunes = (json) => {
+  fs.writeFile('./data/fortunes.json', JSON.stringify(json, undefined, 2), err => console.log(err));
+};
+
 app.post('/fortunes', (req, res) => {
-  console.log(req.body)
 
   const { message, lucky_number, spirit_animal } = req.body;
 
   const fortune_ids = fortunes.map(f => f.id);
 
-  const fortune = {
+  const new_fortunes = fortunes.concat({
+    //take list of ids, find the max, and increment by 1. If undefined, return 0, then add 1
     id: (fortune_ids.length > 0 ? Math.max(...fortune_ids) : 0) + 1,
     message,
     lucky_number,
     spirit_animal
-  };
+  });
 
-  const new_fortunes = fortunes.concat(fortune);
-
-  fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes, undefined, 2), err => console.log(err));
+  writeFortunes(new_fortunes);
 
   res.json(new_fortunes);
 
 });
 
+app.put('/fortunes/:id', (req, res) => {
+  const { id } = req.params;
+  // const { message, lucky_number, spirit_animal } = req.body;
+
+  //maintains reference to original fortunes object, so edits to the reference will modify the original object
+  const old_fortune = fortunes.find(f => f.id == id);
+
+  // if (message) old_fortune.message = message;
+  // if (lucky_number) old_fortune.lucky_number = lucky_number;
+  // if (spirit_animal) old_fortune.spirit_animal = spirit_animal;
+
+  ['message', 'lucky_number', 'spirit_animal'].forEach(key => {
+    if (req.body[key]) old_fortune[key] = req.body[key];
+  });
+
+  writeFortunes(fortunes);
+
+  res.json(fortunes);
+});
+
 module.exports = app;
 
 // curl -H "Content-Type: application/json" -X POST -d '{"message": "Hello", "lucky_number": 5, "spirit_animal": "Dog"}' http://localhost:3000/fortunes
+// OR be normal and use Postman
